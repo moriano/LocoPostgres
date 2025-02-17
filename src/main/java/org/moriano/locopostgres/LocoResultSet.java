4d1595c0fc2e375786bc64d208508bc5d2f955f9
moriano <moriano@gmail.com>
2025-02-17 19:29:49 +1100

    Mini fix, thanks unit tests

--- src/main/java/org/moriano/locopostgres/LocoResultSet.java
+++ src/main/java/org/moriano/locopostgres/LocoResultSet.java
@@ -187,7 +187,7 @@ public class LocoResultSet implements ResultSet {
                 }
                 return result;
             } else {
-                throw new SQLException("Ouch! I cannot process bytes! " + ByteUtil.prettyPrint(rawBytes));
+                return rawBytes;
             }
         }
     }
